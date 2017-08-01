e9b54d0978789f565f3b00d7e1b1e9c291d5435b
KorzunAV <KorzunAV@gmail.com>
2017-08-01 14:19:01 +0300

    Added GetFollowers / GetFollowing

--- Sources/Ditch/OperationManager.cs
+++ Sources/Ditch/OperationManager.cs
@@ -42,15 +42,6 @@ namespace Ditch
         }
 
         /// <summary>
-        /// Get global dinamic properties
-        /// </summary>
-        /// <returns></returns>
-        public JsonRpcResponse<DynamicGlobalProperties> GetDynamicGlobalProperties()
-        {
-            return _webSocketManager.GetRequest<DynamicGlobalProperties>(DynamicGlobalProperties.Reques);
-        }
-
-        /// <summary>
         /// Get user accounts by user names
         /// </summary>
         /// <returns></returns>
@@ -127,5 +118,129 @@ namespace Ditch
 
             return transaction;
         }
+
+        //https://github.com/steemit/steem/blob/master/libraries/wallet/include/steemit/wallet/wallet.hpp
+
+        /// <summary>
+        /// Returns the block chain's rapidly-changing properties.
+        /// The returned object contains information that changes every block interval
+        /// such as the head block number, the next witness, etc.
+        /// @see \c get_global_properties() for less-frequently changing properties
+        /// </summary>
+        /// <returns>the dynamic global properties</returns>
+        public JsonRpcResponse<DynamicGlobalProperties> GetDynamicGlobalProperties()
+        {
+            return _webSocketManager.GetRequest<DynamicGlobalProperties>(DynamicGlobalProperties.Reques);
+        }
+
+        #region follow_api
+
+        public JsonRpcResponse<List<FollowInfo>> GetFollowers(string to, string start, FollowType followType, UInt16 limit = 10)
+        {
+            return _webSocketManager.GetRequest<List<FollowInfo>>("call", FollowInfo.Api, "get_followers", new object[] { to, start, followType.ToString().ToLower(), limit });
+        }
+
+        public JsonRpcResponse<List<FollowInfo>> GetFollowing(string to, string start, FollowType followType, UInt16 limit = 10)
+        {
+            return _webSocketManager.GetRequest<List<FollowInfo>>("call", FollowInfo.Api, "get_following", new object[] { to, start, followType.ToString().ToLower(), limit });
+        }
+
+        #endregion
+
+        #region wallet api
+
+        #endregion wallet api
+
+        #region key api
+        //(import_key)
+        //(suggest_brain_key)
+        //(list_keys)
+        //(get_private_key)
+        //(get_private_key_from_password)
+        //(normalize_brain_key)
+        #endregion key api
+
+        #region query api
+        //(info)
+        //(list_my_accounts)
+        //(list_accounts)
+        //(list_witnesses)
+        //(get_witness)
+        //(get_account)
+        //(get_block)
+        //(get_ops_in_block)
+        //(get_feed_history)
+        //(get_conversion_requests)
+        //(get_account_history)
+        //(get_state)
+        //(get_withdraw_routes)
+        #endregion query api
+
+        #region transaction api
+        //(create_account)
+        //(create_account_with_keys)
+        //(create_account_delegated)
+        //(create_account_with_keys_delegated)
+        //(update_account)
+        //(update_account_auth_key)
+        //(update_account_auth_account)
+        //(update_account_auth_threshold)
+        //(update_account_meta)
+        //(update_account_memo_key)
+        //(delegate_vesting_shares)
+        //(update_witness)
+        //(set_voting_proxy)
+        //(vote_for_witness)
+        //(follow)
+        //(transfer)
+        //(escrow_transfer)
+        //(escrow_approve)
+        //(escrow_dispute)
+        //(escrow_release)
+        //(transfer_to_vesting)
+        //(withdraw_vesting)
+        //(set_withdraw_vesting_route)
+        //(convert_sbd)
+        //(publish_feed)
+        //(get_order_book)
+        //(get_open_orders)
+        //(create_order)
+        //(cancel_order)
+        //(post_comment)
+        //(vote)
+        //(set_transaction_expiration)
+        //(challenge)
+        //(prove)
+        //(request_account_recovery)
+        //(recover_account)
+        //(change_recovery_account)
+        //(get_owner_history)
+        //(transfer_to_savings)
+        //(transfer_from_savings)
+        //(cancel_transfer_from_savings)
+        //(get_encrypted_memo)
+        //(decrypt_memo)
+        //(decline_voting_rights)
+        //(claim_reward_balance)
+        #endregion transaction api
+
+        #region  private message api
+        //(send_private_message)
+        //(get_inbox)
+        //(get_outbox)
+        #endregion  private message api
+
+        #region helper api
+        //(get_prototype_operation)
+        //(serialize_transaction)
+        //(sign_transaction)
+
+        //(network_add_nodes)
+        //(network_get_connected_peers)
+
+        //(get_active_witnesses)
+        //(get_miner_queue)
+        //(get_transaction)
+        #endregion  helper api
     }
 }
